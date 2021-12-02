02d1066789c8f2ad188099b5d0844687e382125c
Trần Cường <trancuongitus@gmail.com>
2021-12-02 16:06:27 +0700

    CRUD and Authorization for User
    Các chức năng cho User.

--- src/main/java/cybersoft/java14/backend/crm/servlet/UserServlet.java
+++ src/main/java/cybersoft/java14/backend/crm/servlet/UserServlet.java
@@ -1,16 +1,113 @@
 package cybersoft.java14.backend.crm.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import cybersoft.java14.backend.crm.model.User;
+import cybersoft.java14.backend.crm.service.RoleService;
+import cybersoft.java14.backend.crm.service.UserService;
+import cybersoft.java14.backend.crm.util.JspConst;
 import cybersoft.java14.backend.crm.util.UrlConst;
 
 @WebServlet(name = "userServlet", urlPatterns = {
 		UrlConst.USER,
-		UrlConst.USER_LIST,
+		UrlConst.USER_DELETE,
 		UrlConst.USER_CREATE,
 		UrlConst.USER_AUTHOR
 })
 public class UserServlet extends HttpServlet {
+	UserService userService;
+	RoleService roleService;
 	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		userService = new UserService();
+		roleService = new RoleService();
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String servletPath = req.getServletPath();
+		
+		switch (servletPath) {
+		case UrlConst.USER:
+			req.setAttribute("roles", roleService.getRoles());
+			req.setAttribute("users", userService.getUsers());
+			req.getRequestDispatcher(JspConst.USER_LIST).forward(req, resp);
+			break;
+		case UrlConst.USER_AUTHOR:
+			String email = req.getParameter("email");
+			req.setAttribute("user", userService.getUserByEmail(email));
+			req.setAttribute("roles", roleService.getRoles());
+			req.getRequestDispatcher(JspConst.USER_AUTHOR).forward(req, resp);
+		default:
+			break;
+		}
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String servletPath = req.getServletPath();
+		req.setCharacterEncoding("UTF-8");
+		
+		switch (servletPath) {
+		case UrlConst.USER_CREATE:
+			User user = new User();
+			user.setName(req.getParameter("user_name"));
+			user.setEmail(req.getParameter("user_email"));
+			user.setPassword(req.getParameter("user_pass"));
+			user.setPhone(req.getParameter("user_phone"));
+			user.setAddress(req.getParameter("user_address"));
+			
+			String roleName = req.getParameter("user_role");
+			for (int i = 0; i < roleService.getRoles().size(); ++i) {
+				if (roleService.getRoles().get(i).getName().equals(roleName)) {
+					user.setRole(roleService.getRoles().get(i));
+					break;
+				}
+			}
+			
+			userService.addUser(user);
+			break;
+			
+		case UrlConst.USER_DELETE:
+			String[] selected = req.getParameterValues("chkSelected");
+			int countRows = userService.deleteUser(selected);
+			System.out.println(countRows + " rows has been deleted!");
+			break;
+			
+		case UrlConst.USER_AUTHOR:
+			User newUser = new User();
+			String email = req.getParameter("email");
+			newUser.setName(req.getParameter("user_name"));
+			newUser.setEmail(req.getParameter("user_email"));
+			newUser.setPhone(req.getParameter("user_phone"));
+			newUser.setAddress(req.getParameter("user_address"));
+			
+			String newRoleName = req.getParameter("user_role");
+			for (int i = 0; i < roleService.getRoles().size(); ++i) {
+				if (roleService.getRoles().get(i).getName().equals(newRoleName)) {
+					newUser.setRole(roleService.getRoles().get(i));
+					break;
+				}
+			}
+			
+			if (userService.updateUser(email, newUser)) {
+				System.out.println("Update successfully!");
+			} else {
+				System.out.println("Error!");
+			}
+			
+		default:
+			break;
+		}
+		
+		resp.sendRedirect(req.getContextPath() + UrlConst.USER);
+	}
 }
--- src/main/webapp/WEB-INF/views/layout/navbar.jsp
+++ src/main/webapp/WEB-INF/views/layout/navbar.jsp
@@ -12,7 +12,7 @@
             <div class="navbar-collapse collapse" id="navbarsExample03">
                 <ul class="nav navbar-nav flex">
                     <li class="nav-item">
-                        <a class="nav-link active" href='<c:url value="home" />'>
+                        <a class="nav-link active" href="/crm/home">
                             Home</a>
                     </li>
                     <li class="nav-item dropdown">
@@ -26,6 +26,10 @@
                             <a class="dropdown-item" href="project/project-statistics">Statistics</a>
                         </div>
                     </li>
+                    <li class="nav-item">
+                    	<a href="/crm/user" class="nav-link">User Management</a>
+                    </li>
+                    <!-- 
                     <li class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
                             User
@@ -36,6 +40,7 @@
                             <a class="dropdown-item" href="user/user-authorization">Authorization</a>
                         </div>
                     </li>
+                     -->
                     <li class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
                             Task
