d1cb0163110a26ce62e42f72f5876a876cad148f
Trần Cường <trancuongitus@gmail.com>
2021-12-07 23:46:05 +0700

    Final update DB

--- Database/DataCRM.sql
+++ Database/DataCRM.sql
@@ -18,6 +18,16 @@ values
     (3, 'Trần Văn B', 'TVB@gmail.com', 'emp123', '0xxxxxxxxx', 'Hà Nội');
     
 insert into
+	crm_user(user_role, user_name, email, usr_password, phone, address)
+values
+	(3, 'Rhonda Popworth', 'rpopworth0@edublogs.org', 'admin123', '0651654892', 'Vale Maceiras'),
+    (3, 'Joseph Pember', 'jpember1@hp.com', 'admin123', '0331657637', 'Jiesheng'),
+    (3, 'Lindsay Tatersale', 'ltatersale2@altervista.org', 'admin123', '0575902507', 'Tsumeb'),
+    (3, 'Alexandro Pocklington', 'apocklington3@deviantart.com', 'leader123', '0896519416', 'Changqi'),
+    (3, 'Ashely Witherow', 'awitherow4@4shared.com', 'emp123', '0462721505', 'Busia'),
+    (3, 'Everett Dicey', 'edicey5@netlog.com', 'emp123', '0893763254', 'Solingen');
+    
+insert into
 	crm_status(status_name, status_description)
 values
     ('Not yet started', 'Công việc chưa được bắt đầu.'),
@@ -34,13 +44,24 @@ values
 insert into
 	crm_task(assignee, task_name, task_description, start_at, end_at, task_status, task_project)
 values
-	(3, 'Create database', 'Create database for project', '2021-11-21', '2021-11-22', 3, 1),
-    (3, 'Mock data', 'Fake data for database of project', '2021-11-23', '2021-11-24', 3, 1),
-    (4, 'CRUD ', 'CRUD for project', '2021-11-25', '2021-12-2', 3, 1),
-    (4, 'Testing', 'Testing for project', '2021-12-3', '2021-12-5', 2, 1),
-    (3, 'Project Structure', 'Create project and download libraries.', '2021-12-7', '2021-12-8', 1, 2),
-    (4, 'Coding', 'Coding project.', '2021-12-9', '2021-12-19', 1, 2),
-    (4, 'Release', 'Test and release', '2021-12-20', '2021-12-21', 1, 2),
-    (3, 'Create database', 'Create database and mock data', '2021-10-1', '2021-10-3', 3, 3),
-    (3, 'Coding admin page', 'Admin page for website', '2021-10-4', '2021-11-1', 3, 3),
-    (4, 'Coding client page', 'Client page for website', '2021-10-4', '2021-11-1', 3, 3);
+	(5, 'Create database', 'Create database for project', '2021-11-21', '2021-11-22', 3, 1),
+    (5, 'Mock data', 'Fake data for database of project', '2021-11-23', '2021-11-24', 3, 1),
+    (6, 'CRUD ', 'CRUD for project', '2021-11-25', '2021-12-2', 3, 1),
+    (6, 'Testing', 'Testing for project', '2021-12-3', '2021-12-5', 2, 1),
+    (5, 'Project Structure', 'Create project and download libraries.', '2021-12-7', '2021-12-8', 1, 2),
+    (6, 'Coding', 'Coding project.', '2021-12-9', '2021-12-19', 1, 2),
+    (6, 'Release', 'Test and release', '2021-12-20', '2021-12-21', 1, 2),
+    (5, 'Create database', 'Create database and mock data', '2021-10-1', '2021-10-3', 3, 3),
+    (5, 'Coding admin page', 'Admin page for website', '2021-10-4', '2021-11-1', 3, 3),
+    (6, 'Coding client page', 'Client page for website', '2021-10-4', '2021-11-1', 3, 3);
+    
+insert into 
+	crm_project_users(project_id, user_id)
+values
+	(1, 5),
+    (1, 6),
+    (2, 5),
+    (2, 6),
+    (3, 5),
+    (3 ,6),
+	(3, 8);
